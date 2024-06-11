089c9ab471b50efcb6ce231a25f66d036b0e65cd
jose angel flores <joseangelflores@joses-MacBook-Air.local>
2024-06-11 11:08:48 -0600

    profile

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -55,3 +55,14 @@ export const login = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
+export const logout = (req, res) => {
+  res.cookie('token', "", {
+     expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
+
+export const profile = (req, res) => {
+  res.send('profile')
+}
