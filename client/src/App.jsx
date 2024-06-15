1b2f5b324227d9e236aa72145ae0edd7e0853bc6
jose angel flores <joseangelflores@joses-MacBook-Air.local>
2024-06-15 17:40:51 -0600

    ajustes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
-  return(
-    <div>
-      <h1 className ="font-bold text-4xl">hOLA MUNDO</h1>
-    </div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home page</h1>} />
+        <Route path='/login' element={<h1>Home login</h1>} />
+        <Route path='/register' element={<h1>Home register</h1>} />
+        <Route path='/profile' element={<h1>Home profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- server/src/schemas/auth.schema.js
+++ server/src/schemas/auth.schema.js
@@ -4,18 +4,27 @@ export const registerSchema = z.object({
   username: z.string({
     required_error: 'Username is requiered'
   }),
+  email: z.string({
+
+  }).email({
+    message: 'Invalid email'
+  }).optional(),
   password: z.string({
     required_error: 'Password is requiered'
   }).min(6,{
     message: "password must be at least 6 characters"
   })
-
 })
 
 export const loginSchema = z.object({
   username: z.string({
     required_error: 'Username is requiered'
   }),
+  email: z.string({
+
+  }).email({
+    message: 'Invalid email'
+  }).optional(),
   password: z.string({
     required_error: 'Password is requiered'
   }).min(6,{
