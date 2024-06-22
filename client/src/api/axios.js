9e63125fd64a1c3adfb72dd70377d630de703d59
jose angel flores <joseangelflores@joses-MacBook-Air.local>
2024-06-22 17:25:46 -0600

    pruebas

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000/api'  // URL local para desarrollo
-  : 'https://api.example.com/api';  // URL de producción
-
+const API_URL = 'http://localhost:3000/api'
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true
