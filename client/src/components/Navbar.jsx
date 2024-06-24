dd79dd0475a3686f1eb99a2ac00995f29654fc6f
jose angel flores <joseangelflores@joses-MacBook-Air.local>
2024-06-24 15:45:01 -0600

    ajustes ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -135,7 +135,7 @@ const Navbar = (() => {
         </div>
       </div>
       {/* Mobile */}
-      <div className="sm:hidden transition-all transform" id="mobile-menu" style={{ display: statusMenu ? 'block' : 'none' }}>
+      <div className={`sm:hidden transition-all transform ${statusMenu ? 'block' : 'hidden'}`}>
         <div className="space-y-1 px-2 pb-3 pt-2">
           <Link to="/" onClick={openMenu} className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
             Home
